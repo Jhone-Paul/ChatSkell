07eb8f831a82ee8797119b074deda168c6beda81
John <26047888@sun.ac.za>
2025-03-01 17:54:46 +0200

    renamed Client.hs to chatskellbot

---
+++ chatskellbot.hs
@@ -0,0 +1,78 @@
+{-# OPTIONS_GHC -Wno-missing-signatures #-}
+
+{- Third step: interpreting commands -}
+
+import Data.List                      -- base
+import System.Exit                    --
+import System.IO                      --
+import qualified Network.Socket as N  -- network
+
+-- Configuration options
+myServer = "192.168.0.151" :: String
+myPort   = 6667 :: N.PortNumber
+myChan   = "#tutbot-testing" :: String
+myNick   = "tutbot" :: String
+
+-- Toplevel program
+main :: IO ()
+main = do
+    h <- connectTo myServer myPort
+    write h "NICK" myNick
+    write h "USER" (myNick ++ " 0 * :tutorial bot")
+    waitForWelcome h  -- Wait until 001 message is received
+    write h "JOIN" myChan
+    listen h
+
+-- Connect to a server given its name and port number
+connectTo :: N.HostName -> N.PortNumber -> IO Handle
+connectTo host port = do
+    addr : _ <- N.getAddrInfo Nothing (Just host) (Just (show port))
+    sock <- N.socket (N.addrFamily addr) (N.addrSocketType addr) (N.addrProtocol addr)
+    N.connect sock (N.addrAddress addr)
+    N.socketToHandle sock ReadWriteMode
+
+-- Send a message to a handle
+write :: Handle -> String -> String -> IO ()
+write h cmd args = do
+    let msg = cmd ++ " " ++ args ++ "\r\n"
+    hPutStr h msg          -- Send message on the wire
+    putStr ("> " ++ msg)   -- Show sent message on the command line
+
+-- Wait for the server's welcome message before joining a channel
+waitForWelcome :: Handle -> IO ()
+waitForWelcome h = do
+    line <- hGetLine h
+    putStrLn line
+    if " 001 " `isInfixOf` line  -- Check if it's the welcome message
+        then return ()
+        else waitForWelcome h  -- Keep waiting until 001 is received
+
+-- Process each line from the server
+listen :: Handle -> IO ()
+listen h = forever $ do
+    line <- hGetLine h
+    putStrLn line
+    let s = init line
+    if isPing s then pong s else eval h (clean s)
+  where
+    forever :: IO () -> IO ()
+    forever a = do a; forever a
+
+    clean :: String -> String
+    clean = drop 1 . dropWhile (/= ':') . drop 1
+
+    isPing :: String -> Bool
+    isPing x = "PING :" `isPrefixOf` x
+
+    pong :: String -> IO ()
+    pong x = write h "PONG" (':' : drop 6 x)
+
+-- Dispatch a command
+eval :: Handle -> String -> IO ()
+eval h "!quit" = write h "QUIT" ":Exiting" >> exitSuccess
+eval h x | "!id " `isPrefixOf` x = privmsg h (drop 4 x)
+eval _ _ = return ()  -- ignore everything else
+
+-- Send a privmsg to the channel
+privmsg :: Handle -> String -> IO ()
+privmsg h msg = write h "PRIVMSG" (myChan ++ " :" ++ msg)
