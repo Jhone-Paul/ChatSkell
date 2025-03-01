eda2830038aee04de7a4d284d7293a15281a6c3d
John <26047888@sun.ac.za>
2025-03-01 18:47:56 +0200

    added blackjack to the chatbot

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # ChatSkell
-An IRC bot written in Haskell, based off of the project guid found on the [haskell wiki](https://wiki.haskell.org/Roll_your_own_IRC_bot#). I have edited the bot slightly to be used on a InspIRCd instance I run on my home lab :)
+An IRC bot written in Haskell, based off of the project guide found on the [haskell wiki](https://wiki.haskell.org/Roll_your_own_IRC_bot#). I have edited the bot for more personal use cases and to be used on a InspIRCd instance I run on my home lab :)
 
 I also though maybe I should create my own IRC server but that seems like a project for another date (and for a languauge i am more comfortable in!)
--- chatskellbot.hs
+++ chatskellbot.hs
@@ -4,14 +4,28 @@
 
 import Data.List                      -- base
 import System.Exit                    --
-import System.IO                      --
+import System.IO (Handle, hPutStrLn, hPutStr, hGetLine, IOMode(ReadWriteMode))
+import System.IO.Unsafe (unsafePerformIO) --
 import qualified Network.Socket as N  -- network
+import System.Random (randomRIO)
+import GHC.Integer (Integer)
+import Control.Monad (replicateM)
+import qualified Data.Map as Map
+import Data.Maybe (fromMaybe)
+import Data.IORef
 
 -- Configuration options
 myServer = "192.168.0.151" :: String
 myPort   = 6667 :: N.PortNumber
 myChan   = "#tutbot-testing" :: String
 myNick   = "tutbot" :: String
+type GameState = Map.Map String [Int]
+
+-- Global IORef to store active games
+gamesRef :: IORef GameState
+{-# NOINLINE gamesRef #-}
+gamesRef = unsafePerformIO (newIORef Map.empty)
+
 
 -- Toplevel program
 main :: IO ()
@@ -71,8 +85,73 @@ listen h = forever $ do
 eval :: Handle -> String -> IO ()
 eval h "!quit" = write h "QUIT" ":Exiting" >> exitSuccess
 eval h x | "!id " `isPrefixOf` x = privmsg h (drop 4 x)
+eval h x | "!bj" `isPrefixOf` x = startBlackjack h (drop 4 x)
+eval h x | "!hit" `isPrefixOf` x = playerHit h (drop 5 x)
+eval h x | "!stand" `isPrefixOf` x = playerStand h (drop 7 x)
 eval _ _ = return ()  -- ignore everything else
 
 -- Send a privmsg to the channel
 privmsg :: Handle -> String -> IO ()
 privmsg h msg = write h "PRIVMSG" (myChan ++ " :" ++ msg)
+
+getRandomNumber :: IO Int
+getRandomNumber = randomRIO (1, 11)
+
+-- Start a new game
+startBlackjack :: Handle -> String -> IO ()
+startBlackjack h user = do
+    hand <- replicateM 2 getRandomNumber  -- Get two random cards
+    modifyIORef gamesRef (Map.insert user hand)
+    let total = sum hand
+    privmsg h (user ++ ", you drew: " ++ show hand ++ ". Total: " ++ show total)
+    if total == 21
+        then privmsg h (user ++ " Blackjack! You win!") >> endGame user
+        else privmsg h (user ++ " !hit or !stand?")
+
+-- Player chooses to hit
+playerHit :: Handle -> String -> IO ()
+playerHit h user = do
+    gameState <- readIORef gamesRef
+    case Map.lookup user gameState of
+        Nothing -> privmsg h (user ++ " No active game. Start with !bj")
+        Just hand -> do
+            newCard <- getRandomNumber
+            let newHand = hand ++ [newCard]
+                total = sum newHand
+            modifyIORef gamesRef (Map.insert user newHand)
+            privmsg h (user ++ ", you drew: " ++ show newCard ++ ". Total: " ++ show total)
+            if total > 21
+                then privmsg h (user ++ " Bust! You lose!") >> endGame user
+                else privmsg h (user ++ " Hit or Stand?")
+
+-- Player chooses to stand, dealer plays
+playerStand :: Handle -> String -> IO ()
+playerStand h user = do
+    gameState <- readIORef gamesRef
+    case Map.lookup user gameState of
+        Nothing -> privmsg h (user ++ " No active game. Start with !bj")
+        Just playerHand -> do
+            dealerHand <- playDealer []
+            let playerTotal = sum playerHand
+                dealerTotal = sum dealerHand
+            privmsg h ("Dealer drew: " ++ show dealerHand ++ ". Total: " ++ show dealerTotal)
+            if dealerTotal > 21 || playerTotal > dealerTotal
+                then privmsg h (user ++ " You win!")
+                else if dealerTotal == playerTotal
+                    then privmsg h (user ++ " It's a tie!")
+                    else privmsg h (user ++ " Dealer wins!")
+            endGame user
+
+-- Dealer AI: Hits on any total below 17
+playDealer :: [Int] -> IO [Int]
+playDealer hand = do
+    let total = sum hand
+    if total >= 17
+        then return hand
+        else do
+            newCard <- getRandomNumber
+            playDealer (hand ++ [newCard])
+
+-- End game, remove player from active games
+endGame :: String -> IO ()
+endGame user = modifyIORef gamesRef (Map.delete user)
